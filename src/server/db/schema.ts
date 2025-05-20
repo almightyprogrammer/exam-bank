cc4d89c30f6dbc943c0fa2dc5e6109fba257e458
almightyprogrammer <lehman0308@gmail.com>
2025-05-20 23:02:35 +1200

    update

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -3,11 +3,8 @@ import { int, bigint, text, singlestoreTable } from "drizzle-orm/singlestore-cor
 
 
 export const users = singlestoreTable("users_table", {
-
   id: bigint("id", { mode: "bigint" }).primaryKey().autoincrement(),
-
   name: text("name"),
-
   age: int("age"),
-
 });
+
